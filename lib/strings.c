c63aa44a247329e18717be64d3afab15b05b7cc4
Jude Capachietti <judecapachietti@gmail.com>
2023-11-24 15:50:27 -1000

    3.18

--- lib/strings.c
+++ lib/strings.c
@@ -1,14 +1,9 @@
 #include <emscripten.h>
 #include <stdio.h>
 
-
-char * str = "my string variable";
-
-char * getStr() {
-    return str;
-}
-
 int main() {
+    
     printf("WASM Ready!\n");
+
     return 1;
 }
